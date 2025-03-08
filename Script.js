a44e6a24b391e87e397a304eff24837f3f9eb0a8
TAHA VARDA <69712693+tv786@users.noreply.github.com>
2025-03-08 21:33:41 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -166,7 +166,7 @@ function sendToGoogleSheet() {
         invoiceDate, buyerName, buyerAddress, items, addAmount, paymentStatus, remarks, totalInvoiceAmount
     };
 
-    fetch("AKfycbwphrz4j1Qjgj7poiXD4qNx6YXIqBMLSt2r3PuXbB8", {
+    fetch("https://script.google.com/macros/s/AKfycbwSz9SrWVSjf9sgZappg5l653lO8ZXS9uqSPs1Y93y8t_O1meDy2IE_t5T8FuY2UBhV/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
